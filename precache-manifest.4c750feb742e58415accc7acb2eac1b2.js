57ed8fd182a2414173c8d54cf78147aac38a53bb
DarthBenro008 <hkpdev008@gmail.com>
2020-10-11 21:00:02 +0530

    Updates

---
+++ precache-manifest.4c750feb742e58415accc7acb2eac1b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cabd07a8ff374f695377f79ef6be1ce",
+    "url": "/syncure-react/index.html"
+  },
+  {
+    "revision": "96e374b24896497366e3",
+    "url": "/syncure-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2170e3a4ac2d68ea1db1",
+    "url": "/syncure-react/static/css/main.11e16613.chunk.css"
+  },
+  {
+    "revision": "96e374b24896497366e3",
+    "url": "/syncure-react/static/js/2.3094ac43.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/syncure-react/static/js/2.3094ac43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2170e3a4ac2d68ea1db1",
+    "url": "/syncure-react/static/js/main.0808d180.chunk.js"
+  },
+  {
+    "revision": "d76b1d02f960268c7ada",
+    "url": "/syncure-react/static/js/runtime-main.8da198e0.js"
+  },
+  {
+    "revision": "ff489b9e1707dc5614b3a78be341b3a6",
+    "url": "/syncure-react/static/media/Group 3.ff489b9e.svg"
+  },
+  {
+    "revision": "f9f9e1627e10923ea352d66b302f1da2",
+    "url": "/syncure-react/static/media/logo.f9f9e162.png"
+  }
+]);
